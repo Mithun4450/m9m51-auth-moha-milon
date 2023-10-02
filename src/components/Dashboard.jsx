e20680fc2e2d52033133b06c6f7b404eb0f7aa49
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2023-10-02 16:14:36 +0600

    first commit

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,12 @@
+
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h3>This is dashboard</h3>
+            
+        </div>
+    );
+};
+
+export default Dashboard;
---
+++ src/components/Orders.jsx
@@ -0,0 +1,20 @@
+
+
+const Orders = () => {
+    return (
+        <div>
+            <h3>Your pesonal orders:</h3>
+            <ul>
+                <li>Items-1</li>
+                <li>Items-2</li>
+                <li>Items-3</li>
+                <li>Items-4</li>
+                <li>Items-5</li>
+               
+            </ul>
+            
+        </div>
+    );
+};
+
+export default Orders;
